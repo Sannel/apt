79f2cfd07477c974c73a0db99e31405a53da9012
Adam Holt <holtsoftware@outlook.com>
2023-06-16 23:31:58 -0600

    updates

--- prepare.ps1
+++ prepare.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 dpkg-scanpackages --multiversion --arch arm64 pool/ > dists/stable/main/binary-arm64/Packages
-cat dists/stable/main/binary-arm64/Packages | gzip -9 > dists/stable/main/binary-arm64/Packages.gz
+gzip -9 dists/stable/main/binary-arm64/Packages
 
 Set-Location dists/stable
 ../../generate-release.sh > Release
