705db220434b4d690ae72c1f6892676ad075892b
Adam Holt <holtsoftware@outlook.com>
2023-06-17 14:34:30 -0600

    update

--- prepare.ps1
+++ prepare.ps1
@@ -1,1 +1,6 @@
 #!/usr/bin/env pwsh
+$num = [System.DateTime]::Now.ToString("yyyyMMddhhmm")
+$snapName = "sannel-snapshot-$num"
+aptly snapshot create $snapName from repo sannel-repository
+aptly publish switch sannel $snapName
+cp -rvf ~/.aptly/public .
