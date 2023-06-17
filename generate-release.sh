a10a8f6ad0e2f12171e7681b4234e2e0c1680d6d
Adam Holt <holtsoftware@outlook.com>
2023-06-17 12:47:27 -0600

    updates

--- generate-release.sh
+++ generate-release.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 set -e
 
-do_hash() {
-    HASH_NAME=$1
-    HASH_CMD=$2
-    echo "${HASH_NAME}:"
-    for f in $(find -type f | grep Packages); do
-        f=$(echo $f | cut -c3-) # remove ./ prefix
-        if [ "$f" = "Release" ]; then
-            continue
-        fi
-        echo " $(${HASH_CMD} ${f}  | cut -d" " -f1) $(wc -c $f)"
-    done
-}
-
 cat << EOF
 Origin: Sannel Software, L.L.C.
 Label: sannel
@@ -23,9 +10,4 @@ Version: 1.0
 Architectures: arm64
 Components: main
 Description: Apt repo for Sannel Software Projects
-Date: $(date -Ru)
 EOF
-do_hash "MD5Sum" "md5sum"
-do_hash "SHA1" "sha1sum"
-do_hash "SHA256" "sha256sum"
-do_hash "SHA512" "sha512sum"
--- prepare.ps1
+++ prepare.ps1
@@ -1,7 +1,17 @@
 #!/usr/bin/env pwsh
-dpkg-scanpackages --arch arm64 pool/ > dists/stable/main/binary-arm64/Packages
-gzip -9 -f dists/stable/main/binary-arm64/Packages
+apt-ftparchive --arch arm64 packages pool > dists/stable/main/binary-arm64/Packages
+gzip -9 -k -f dists/stable/main/binary-arm64/Packages
 
-Set-Location dists/stable
-../../generate-release.sh > Release
-Set-Location ../..
+$version = [System.DateTime]::Now.ToString("yyyy.MM.dd.hh.mm");
+
+$content = "Origin: Sannel Software, L.L.C.
+Label: sannel
+Suite: stable
+Codename: stable
+Version: $version
+Architectures: arm64
+Components: main
+Description: Apt repo for Sannel Software Projects
+";
+Set-Content ./dists/stable/Release -Value $content
+apt-ftparchive release . >> ./dists/stable/Release
