7c79cd8b92e338fdb19d94d47c9d74b683fc2f33
Adam Holt <holtsoftware@outlook.com>
2023-06-17 13:16:15 -0600

    update

--- prepare.ps1
+++ prepare.ps1
@@ -1,6 +1,8 @@
 #!/usr/bin/env pwsh
 apt-ftparchive --arch arm64 packages pool > dists/stable/main/binary-arm64/Packages
 gzip -9 -k -f dists/stable/main/binary-arm64/Packages
+apt-ftparchive --arch armhf packages pool > dists/stable/main/binary-armhf/Packages
+gzip -9 -k -f dists/stable/main/binary-armhf/Packages
 
 $version = [System.DateTime]::Now.ToString("yyyy.MM.dd.hh.mm");
 
@@ -9,7 +11,7 @@ Label: sannel
 Suite: stable
 Codename: stable
 Version: $version
-Architectures: arm64
+Architectures: arm64 armhf
 Components: main
 Description: Apt repo for Sannel Software Projects";
 Set-Content ./dists/stable/Release -Value $content
